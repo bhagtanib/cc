ce76a22987182ff4785460a36695deecae142239
bhagtanib <b.bhagtani10@gmail.com>
2024-04-24 00:34:21 -0700

    Added Heading

--- src/Journey.js
+++ src/Journey.js
@@ -11,44 +11,47 @@ import {
 const Journey = () => {
   const tables = [];
   return (
-    <div>
+    <div className="journey-page">
       <h1>From source to solution: My Data Journey </h1>
       <div className="journey-container">
         <div className="journey-knowledge-container journey-dialog">
           <span style={{ fontWeight: 600 }}>Heading 1</span>
-          {images1.map((imageName) => (
-            <img
-              key={imageName}
-              src={`${process.env.PUBLIC_URL}/images/${imageName}`}
-              alt="Imagessss"
-            />
-            //console.log(`${process.env.PUBLIC_URL}/images/${imageName}`)
-          ))}
+          <div className="journey-dialog-images">
+            {images1.map((imageName) => (
+              <img
+                key={imageName}
+                src={`${process.env.PUBLIC_URL}/images/${imageName}`}
+                alt="Imagessss"
+              />
+              //console.log(`${process.env.PUBLIC_URL}/images/${imageName}`)
+            ))}
+          </div>
         </div>
         <div className="journey-mid-container journey-dialog">
           <span style={{ fontWeight: 600 }}>Heading 1</span>
-
-          {images2.map((imageName) => (
-            <img
-              key={imageName}
-              src={`${process.env.PUBLIC_URL}/images/${imageName}`}
-              alt="Imagessss"
-            />
-            //console.log(`${process.env.PUBLIC_URL}/images/${imageName}`)
-          ))}
+          <div className="journey-dialog-images">
+            {images2.map((imageName) => (
+              <img
+                key={imageName}
+                src={`${process.env.PUBLIC_URL}/images/${imageName}`}
+                alt="Imagessss"
+              />
+              //console.log(`${process.env.PUBLIC_URL}/images/${imageName}`)
+            ))}
+          </div>
         </div>
         <div className="journey-application-container journey-dialog">
-          <div className={`card-stuff-img1`} >
-            <Card tableData = {workExperience} />
+          <div className={`card-stuff-img1`}>
+            <Card tableData={workExperience} />
           </div>
-          <div className={`card-stuff-img2`} >
-            <Card tableData = {certificate} />
+          <div className={`card-stuff-img2`}>
+            <Card tableData={certificate} />
           </div>
-          <div className={`card-stuff-img3`} >
-            <Card tableData = {projects} />
+          <div className={`card-stuff-img3`}>
+            <Card tableData={projects} />
           </div>
           <div className={`card-stuff-img4`}>
-            <Card  tableData = {factsTable} />
+            <Card tableData={factsTable} />
           </div>
           {/* {Array(4)
             .fill(4)
