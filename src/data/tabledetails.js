deb2f3bf705d1460ce1ad10f4f2b2a1c4d937cf7
Mradul Mourya <mmourya23@my.whitworth.edu>
2024-04-23 23:36:57 -0700

    schema update

--- src/data/tabledetails.js
+++ src/data/tabledetails.js
@@ -1,52 +1,95 @@
 export const workExperience = {
-    heading: ["Work Id", "Company Name", "Start Date", "End Date" ],
+    heading: ["workId", "companyName", "position", "startDate", "endDate", "skillsLearned", "contributionsMade" ],
+    schema: {
+        workId: "uniqueidentifier",
+        companyName: "varchar",
+        position: "varchar",
+        startDate: "date",
+        endDate:  "date",
+        skillsLearned: "varchar",
+        contributionsMade: "varchar",
+    },
     rows: [{
-        workId: "abhe2",
+        workId: "c6181e51-3d10-4f8e-b0e4-7c393b775a28",
         companyName: "Treasury4",
-        startDate: "15 Nov 2021",
-        EndDate:  "Present"
+        position: "Data Analytics Engineer",
+        startDate: "11/15/2021",
+        endDate:  "Present",
+        skillsLearned: "df ",
+        contributionsMade: "varchar",
     },
     {
-        workId: "abhe2",
-        companyName: "F3",
+        workId: "f2cc0cf1-0bf6-4e67-9c13-e4a8bb8b6d47",
+        companyName: "Medcurity",
+        position: "Data Analyst Intern",
         startDate: "15 Nov 2021",
-        EndDate:  "Present"
+        endDate:  "Present",
+        skillsLearned: " ",
+        contributionsMade: " "
     },
     {
-        workId: "abhe2",
-        companyName: "Treasury4",
+        workId: "a3f9e0b3-7c9d-4f46-bd08-3a4c73488972",
+        companyName: "Whitworth University",
         startDate: "15 Nov 2021",
-        EndDate:  "Present"
+        endDate:  "Present",
+        skillsLearned: " ",
+        contributionsMade: " "
     },
     {
-        workId: "abhe2",
+        workId: "9c2e145f-781f-4b36-a831-894722f62c72",
         companyName: "Treasury4",
         startDate: "15 Nov 2021",
-        EndDate:  "Present"
+        endDate:  "Present",
+        skillsLearned: " ",
+        contributionsMade: " "
+    },
+    {
+        workId: "ee2a10e9-2e56-4b52-b507-0a0b1baf8d9f",
+        companyName: "Treasury4",
+        startDate: "15 Nov 2021",
+        endDate:  "Present",
+        skillsLearned: " ",
+        contributionsMade: " "
+    },
+    {
+        workId: "bf6396a4-3704-4d6d-8d72-e08f6327889f",
+        companyName: "Treasury4",
+        startDate: "15 Nov 2021",
+        endDate:  "Present",
+        skillsLearned: " ",
+        contributionsMade: " "
+    },
+    {
+        workId: "ba4f45e8b-7269-42e3-aef2-768d7b19af5d",
+        companyName: "Treasury4",
+        startDate: "15 Nov 2021",
+        endDate:  "Present",
+        skillsLearned: " ",
+        contributionsMade: " "
     }
 ]
 }
 
 export const certificate = {
-    heading: ["CertificationId", "Company Name", "Start Date", "End Date" ],
+    heading: ["CertificationId", "CompanyName", "StartDate", "EndDate" ],
     rows: [{
         CertificationId: "abhe2",
-        companyName: "IBM",
-        startDate: "15 Nov 2021",
+        CompanyName: "IBM",
+        StartDate: "15 Nov 2021",
         EndDate:  "Present"
     },
     {
         CertificationId: "abhe2",
         companyName: "Course Era",
-        startDate: "11 APR 2021",
+        StartDate: "11 APR 2021",
         EndDate:  "Present"
     },
 ]
 }
 export const projects = {
-    heading: ["projectId", "Company Name", "Start Date", "End Date" ],
+    heading: ["projectId", "CompanyName", "StartDate", "EndDate" ],
     rows: [{
-        projectId: "abhe2",
+        ProjectId: "abhe2",
         companyName: "Treasury4",
         startDate: "15 Nov 2021",
         EndDate:  "Present"
@@ -55,9 +98,9 @@ export const projects = {
 ]
 }
 export const factsTable = {
-    heading: ["factsId", "Company Name", "Start Date", "End Date" ],
+    heading: ["FactsId", "CompanyName", "StartDate", "EndDate" ],   
     rows: [{
-        factsId: "ceduicw",
+        FactsId: "ceduicw",
         companyName: "Treasury4",
         startDate: "25 Mar 2022",
         EndDate:  "Present"
